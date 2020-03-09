84471a90031cbd80c70631f7d4fe34e332a24801
hjyun <hjyun@jam2in.com>
2020-03-09 10:20:00 +0900

    LEGAL: Update copyright

--- src/main/java/com/navercorp/arcus/spring/cache/ArcusCacheManager.java
+++ src/main/java/com/navercorp/arcus/spring/cache/ArcusCacheManager.java
@@ -1,6 +1,6 @@
 /*
  * arcus-spring - Arcus as a caching provider for the Spring Cache Abstraction
- * Copyright 2011-2014 NAVER Corp.
+ * Copyright 2019 JaM2in Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
